8fac2feb7701267898eed5279d8276262effcb53
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-05-12 22:47:56 -0400

    added current weather API section

--- Assets/JS-files/script.js
+++ Assets/JS-files/script.js
@@ -7,7 +7,6 @@ $(document).ready(function () {
 
         // Limit search history to last 5 cities
         var last5 = searches.slice(0, 4);
-
         for (var i = 0; i < last5.length; i++) {
             $("#search-history").append($("<p class='city'>").text(last5[i]));
         }
@@ -18,19 +17,57 @@ $(document).ready(function () {
         event.preventDefault();
 
         // Retrieve city value from input
-        var city = $("#city").val().trim();
-        console.log(city)
+        var city = $("#search").val().trim();
 
         // Add city to search history
         searches.push(city);
         localStorage.setItem("searches", JSON.stringify(searches));
-        $("#city").val("");
+        $("#search").val("");
         renderHistory();
 
-        
+        // Pull current weather data from API 
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=c9288340a0d00c00e02bf6e9f809e872",
+            method: "GET"
+        }).then(function (response) {
+            // Convert the temp to fahrenheit
+            // var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+            // var tempLike = (response.main.feels_like - 273.15) * 1.80 + 32;
+
+            // Insert data into current weather section
+            var box = $("#current-weather");
+            console.log(response);
+            box.append($("#city").text(response.name));
+            box.append($("#temp").text("Current Temp (F): " + response.main.temp.toFixed(2)));
+            box.append($("#feels-like").text("Feels Like (F): " + response.main.feels_like.toFixed(2)));
+            box.append($("#humidity").text("Humidity: " + response.main.humidity + "%"));
+            box.append($("#wind").text("Wind Speed: " + response.wind.speed + " mph"));
+            $("#current-weather").append(box);
+        })
+
+        // Pull 5 day forecast data from API
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=c9288340a0d00c00e02bf6e9f809e872",
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+            // Convert the temp to fahrenheit
+            // var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+            // var tempLike = (response.main.feels_like - 273.15) * 1.80 + 32;
+
+            // Insert data into current weather section
+            // var box = $("#current-weather");
+            // console.log(response);
+            // box.append($("#city").text(response.name));
+            // box.append($("#temp").text(tempF));
+            // box.append($("#feels-like").text(tempLike));
+            // box.append($("#humidity").text(response.main.humidity));
+            // box.append($("#wind").text(response.wind.speed));
+            // $("#current-weather").append(box);
+
+        })
     });
 
-    // 
     $(document).on("click", ".city", function () {
         console.log($(this).text());
     });
