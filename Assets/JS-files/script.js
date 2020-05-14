fd2b0b8b75dad620b8e161500e928995e047d5fc
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-05-14 01:13:31 -0400

    changed all http to https in js file to resolve mixed content error

--- Assets/JS-files/script.js
+++ Assets/JS-files/script.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
 
         // Pull current weather data from API 
         $.ajax({
-            url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=c9288340a0d00c00e02bf6e9f809e872",
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=c9288340a0d00c00e02bf6e9f809e872",
             method: "GET"
         }).then(function (response) {
             // Add city to search history
@@ -51,7 +51,7 @@ $(document).ready(function () {
         function getUVindex(cityCoord) {
             // Call current UV data for current city
             $.ajax({
-                url: "http://api.openweathermap.org/data/2.5/uvi?appid=c9288340a0d00c00e02bf6e9f809e872&lat=" + cityCoord[0] + "&lon=" + cityCoord[1],
+                url: "https://api.openweathermap.org/data/2.5/uvi?appid=c9288340a0d00c00e02bf6e9f809e872&lat=" + cityCoord[0] + "&lon=" + cityCoord[1],
                 method: "GET"
             }).then(function (response) {
                 var currentEL = $("#current-weather");
@@ -62,7 +62,7 @@ $(document).ready(function () {
 
         // Pull 5 day forecast data from API
         $.ajax({
-            url: "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=c9288340a0d00c00e02bf6e9f809e872",
+            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=c9288340a0d00c00e02bf6e9f809e872",
             method: "GET"
         }).then(function (response) {
             console.log(response)
@@ -74,7 +74,7 @@ $(document).ready(function () {
             for (var i = 1; i < 6; i++) {
                 var tile = $(".card");
                 var title = $("<h5 class='card-title'>")
-                var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png").attr("style" , "width: 100px;")
+                var img = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png").attr("style" , "width: 100px;")
                 var p1 = $("<p>").addClass("card-text").text("Temp: " + response.list[i].main.temp_max + " °F");
             var p2 = $("<p>").addClass("card-text").text("Humidity: " + response.list[i].main.humidity + "%");
 
